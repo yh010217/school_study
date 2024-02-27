21b2f403a6a2ac11c880228f86ff9bd0f95869ad
FULL8-007 <cyh010217@gmail.com>
2024-02-27 14:35:53 +0900

    어느정도 틀은 완성된 듯?

--- js/showAd.js
+++ js/showAd.js
@@ -1,14 +1,39 @@
-document.querySelector('.advertise_right>img').onclick=function(){
+document.querySelector('#advertise_right_button').onclick=function(){
 
-
-    if(document.querySelector('.advertise_right').innerHTML
-        ===`<img src="img/왼화살표.png">`){
-        document.querySelector('.advertise_right').innerHTML =`<img src="img/오른화살표.png">`;
+    if(document.querySelector('#advertise_right_button')
+            .innerHTML===`<img src="img/왼화살표.png"><p>밀크T<br>학습<br>시작</p>`){
+        document.querySelector('#advertise_right_button')
+            .innerHTML =`<img src="img/오른화살표.png"><p>밀크T<br>학습<br>시작</p>`;
     }
     else {
-        document.querySelector('.advertise_right').innerHTML = `<img src="img/왼화살표.png">`;
+        document.querySelector('#advertise_right_button')
+            .innerHTML = `<img src="img/왼화살표.png"><p>밀크T<br>학습<br>시작</p>`;
     }
-
     document.querySelector('.advertise_right').classList.
-        toggle("showAd");
+    toggle("showAd");
+
 };
+
+
+document.querySelector('#inner_tab_button').onclick=function(){
+    document.querySelector('.inner_tab_wrap').classList.
+    toggle("inner_tab_wrap_open");
+
+    document.querySelector('.inner_tab').classList.
+    toggle("inner_tab_open");
+    document.querySelector('.inner_tab').classList.
+    toggle("inner_tab_close");
+    //innertab 의 크기만큼 for문 돌리고 거기서 open 돼있는거 찾으면 지우고(toggle로 가능한가)
+}
+
+document.querySelector('#nyangi').onclick=function(){
+    document.querySelector('.inner_tab_wrap').classList.
+    toggle("inner_tab_wrap_open");
+
+    document.querySelector('.inner_tab').classList.
+    toggle("inner_tab_open");
+    document.querySelector('.inner_tab').classList.
+    toggle("inner_tab_close");
+    //innertab 의 크기만큼 for문 돌리고 거기서 open 돼있는거 찾으면 지우고(toggle로 가능한가)
+}
+
